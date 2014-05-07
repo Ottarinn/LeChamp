e03e52b4715434cdbb7941688a2884bcfb50be5a
Elli <erlingur13@ru.is>
2014-05-07 21:18:53 +0000

    Added first version of project

---
+++ TestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApp.Startup))]
+namespace TestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
